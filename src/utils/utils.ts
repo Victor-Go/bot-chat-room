e4800eae59969c9848da08dba338a037e5b5dc26
zhipeng.yang.it@hotmail.com <zhipeng.yang.it@hotmail.com>
2022-09-20 11:28:51 +0800

    Use npm package to format timestamp.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,11 @@
+import timestamp from 'time-stamp'
+
 export const randomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 export const getFormattedDate = (timeStamp: number) => {
-  const date = new Date(timeStamp)
   const today = new Date().setHours(0, 0, 0, 0)
-  const year = date.getFullYear()
-  const month = date.getMonth()
-  const day = date.getDate()
-  const hours = date.getHours()
-  const minutes = '0' + date.getMinutes()
-  const seconds = '0' + date.getSeconds()
-  const formattedTime = hours + ':' + minutes.slice(-2) + ':' + seconds.slice(-2)
-  return today === new Date(timeStamp).setHours(0, 0, 0, 0) ? formattedTime : `${year}-${month + 1}-${day} ${formattedTime}`
+  const time = new Date(timeStamp)
+  return today === new Date(timeStamp).setHours(0, 0, 0, 0) ? timestamp('HH:mm:ss', time) : timestamp('YYYY-MM-DD HH:mm:ss', time)
 }
